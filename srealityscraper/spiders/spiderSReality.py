04c1a368adf7bf607e02f4ecccb4711b5f5d2667
Frantisek <frantisek.rehak@tul.cz>
2022-10-11 17:17:44 +0200

    paging done

--- srealityscraper/spiders/spiderSReality.py
+++ srealityscraper/spiders/spiderSReality.py
@@ -1,33 +1,38 @@
+import json
+
 import scrapy
+
 
 class WebSpider(scrapy.Spider):
     name = 'srealityspider'
-    start_urls = ['https://www.sreality.cz/hledani/byty/prodej?_escaped_fragment_=']
+    start_urls = ['https://www.sreality.cz/hledani/byty/prodej?_escaped_fragment_=&strana=1']
     custom_settings = {
-        'USER_AGENT':'Frantisek Rehak',
+        'USER_AGENT': 'Frantisek Rehak',
         'AUTOTHROTTLE_ENABLED': True,
         'AUTOTHROTTLE_START_DELAY': 0.5,
         'CLOSESPIDER_ITEMCOUNT': 500,
-        #'DOWNLOAD_DELAY': 0.1,
-        #'RANDOMIZE_DOWNLOAD_DELAY' : False,
+        'CLOSESPIDER_PAGECOUNT': 25
+        # 'DOWNLOAD_DELAY': 0.1,
+        # 'RANDOMIZE_DOWNLOAD_DELAY' : False,
     }
+    page_number = 1
 
     def parse(self, response):
         for properties in response.css("div[class='property ng-scope']"):
             try:
 
-                yield{
+                yield {
                     'title': properties.css("a.title span[class='name ng-binding']::text").get(),
                     'image_url': properties.css("a._2vc3VMce92XEJFrv8_jaeN img::attr(src)").get(),
                 }
             except:
                 print("parse error")
 
+        self.page_number += 1
+        next_page = f'https://www.sreality.cz/hledani/byty/prodej?_escaped_fragment_=&strana={self.page_number}'
 
-        next_page = response.xpath('//a[@class="pagging-next"]').extract_first()
+        yield response.follow(next_page, callback=self.parse)
 
-        if next_page is not None:
-            yield scrapy.Request(next_page, callback=self.parse)
 
 '''
             image
