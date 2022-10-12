498264a90d0204efa375df2332237253d1d46cf7
Frantisek <frantisek.rehak@tul.cz>
2022-10-12 16:57:16 +0200

    database connection and flask setup

---
+++ app.py
@@ -0,0 +1,29 @@
+import psycopg2
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def get_connection():
+    conn = psycopg2.connect(
+        host='localhost',
+        database='postgres',
+        user='postgres',
+        password='postgres'
+    )
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM srealitydata')
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__name__':
+    app.run(debug=True)
